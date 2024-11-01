ea4fab45dc62bc5ec4c5d90521c14e2761c616eb
mlubinskiDE <maciej.lubinski04@gmail.com>
2024-11-01 22:04:13 +0100

    Solved Problem 2

--- mw2383_Assignment01/P2_formatting/p2.c
+++ mw2383_Assignment01/P2_formatting/p2.c
@@ -2,9 +2,16 @@
 
 int main(void){
     
-	/*
-	Add solution here
-	*/
+	int inputSeconds = 55536;
+	int displaySeconds = inputSeconds % 60;
+	int totalMinutes = (inputSeconds - displaySeconds)/60;
+	int displayMinutes = totalMinutes % 60;
+	int displayHours = (totalMinutes - displayMinutes)/60;
+	
+	printf("%d:%d:%d", displayHours, displayMinutes, displaySeconds);
+
+	return 0;
+	
 }
 
 
