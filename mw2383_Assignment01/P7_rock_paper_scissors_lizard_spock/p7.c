8062efd8ed7c226adbc796e7fbeda0a6b93244b1
mlubinskiDE <maciej.lubinski04@gmail.com>
2024-11-01 22:15:24 +0100

    Initial solve for Problem 7. Need to finish TODO's

--- mw2383_Assignment01/P7_rock_paper_scissors_lizard_spock/p7.c
+++ mw2383_Assignment01/P7_rock_paper_scissors_lizard_spock/p7.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 int main() {
-	/*
-	Add solution here
-	*/
+	
+	// declaration of intial variables
+	bool RPPSL = true;
+	char userInput;
+
+	while(RPPSL == true) {
+		
+		//ask user for input and write it onto declared userInput
+		//TODO: sanitize input to one character, avoid double printf, notify user of invalid input
+		printf("Enter the one of the following letters: r, p, s, l, k (or x to exit):");
+		scanf("%c", &userInput);
+		
+		//ensure userInput is lowercase
+		userInput = tolower(userInput);
+		
+		//rock-paper-scissors-lizzard-spock dictionary
+		switch (userInput)
+		{
+		case 'r':
+			printf("Scissors or Lizzard\n");
+			break;
+
+		case 'p':
+			printf("Rock or Spock\n");
+			break;
+
+		case 's':
+			printf("Paper or Lizzard\n");
+			break;
+
+		case 'l':
+			printf("Paper or Spock\n");
+			break;
+
+		case 'k':
+			printf("Rock or Scissors\n");
+			break;
+
+		default:
+				break;
+		};
+		
+		//exit condition
+		if (userInput == 'x') {
+			RPPSL = false;
+		}
+
+	}
     
+	return 0;
+
 }
