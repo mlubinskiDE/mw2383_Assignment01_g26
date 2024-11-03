675f1f46deabb0f6b51100c8f3c1002f71466160
mlubinskiDE <maciej.lubinski04@gmail.com>
2024-11-03 20:40:48 +0100

    Initial solve for Problem 8. Need to implement user input, sanitization and centering the pyramid.

--- mw2383_Assignment01/P8_number_pyramid/p8.c
+++ mw2383_Assignment01/P8_number_pyramid/p8.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
 int main(){  
-	/*
-	Add solution here
-	*/
-    
+
+	// TODO: Center the middle number so that it looks like a pyramid.
+
+	char *pyramidStr[] = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	int userInput = 9;
+
+	int loopTracker= userInput;
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < userInput; i++) { // row itterator
+		k = 0;
+		for (j = 1; j < (loopTracker*2); j++) { // array itterator
+			if (j > loopTracker) {
+				k--;
+			}
+			else {
+				k++;
+			}
+			printf("%s", pyramidStr[k]);
+		}
+		printf("\n");
+		loopTracker--;
+	}
+
+
+
+
+	return 0;
+	
 } 
 
