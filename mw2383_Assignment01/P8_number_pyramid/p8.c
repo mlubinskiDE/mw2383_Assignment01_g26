7e1189003a41e7fb70eab1d36f1ae49dc5c1ee84
mlubinskiDE <maciej.lubinski04@gmail.com>
2024-11-04 21:47:18 +0100

    Centered pyramid. Added user input and input validation

--- mw2383_Assignment01/P8_number_pyramid/p8.c
+++ mw2383_Assignment01/P8_number_pyramid/p8.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include<ctype.h>
 
 int main(){  
 
-	// TODO: Center the middle number so that it looks like a pyramid.
+	
+	char *pyramidStr[] = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"}; // I now realize how unneccesarily complex my solution is. I could have just printed the value of a all encomasing int variable rather than doing this non-scaleable array walk
+	char userInput[32];
+	int userInputValue;
+	bool correctUserInput = false;
 
-	char *pyramidStr[] = {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-	int userInput = 9;
+	printf("Please enter a number between 3 and 9: ");
+	fgets(userInput, sizeof(userInput), stdin);
+	sscanf(userInput, " %d", &userInputValue);
 
-	int loopTracker= userInput;
-	int i;
-	int j;
-	int k;
+	// input validator
+	while (correctUserInput == false) {
+		if (userInputValue > 2 && userInputValue < 10) {
+			correctUserInput = true;
+		}
+		else {
+			printf("Invalid input. Please enter a number between 3 and 9: ");
+			fgets(userInput, sizeof(userInput), stdin);
+			sscanf(userInput, " %d", &userInputValue);
+		}
+	}
+	
+	
+	
+	
+	int loopTracker = userInputValue;
+	for (int i = 0; i < userInputValue; i++) { // row itterator
+		
+		for (int h = 0; h < userInputValue - loopTracker; h++) { // whitespace
+			printf("%s", pyramidStr[0]);
+		}
 
-	for (i = 0; i < userInput; i++) { // row itterator
-		k = 0;
-		for (j = 1; j < (loopTracker*2); j++) { // array itterator
+		int k = 0;
+		for (int j = 1; j < (loopTracker*2); j++) { // array walk
 			if (j > loopTracker) {
 				k--;
 			}
