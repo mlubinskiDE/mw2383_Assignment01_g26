0be3087455b3a210a7ae51bcf9fa4006cb81e29b
Selennsta <selennusta1@outlook.com>
2024-11-13 15:02:59 +0100

    Update p3.c

--- mw2383_Assignment01/P3_relational_Operators/p3.c
+++ mw2383_Assignment01/P3_relational_Operators/p3.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-	int var1, var2;
-    // Input values for var1 and var2
+ int var1, var2;
+    int input_status;
+    // Prompt and check input for var1
     printf("Enter value for var1: ");
-    scanf("%d", &var1);
-    printf("Enter value for var2: ");
-    scanf("%d", &var2);
+    input_status = scanf("%d", &var1);
 
+    if (input_status != 1) { // Check if input was not an integer
+        printf("Invalid input. Please enter an integer value.\n");
+        return 1; // Exit with an error code
+    }
+    // Prompt and check input for var2
+    printf("Enter value for var2: ");
+    input_status = scanf("%d", &var2);
+
+    if (input_status != 1) { // Check if input was not an integer
+        printf("Invalid input. Please enter an integer value.\n");
+        return 1; // Exit with an error code
+    }
     // Check if var1 is at least two times larger than var2
     if (var1 >= 2 * var2) {
         printf("Pasta\nis\nyummy.\n");
