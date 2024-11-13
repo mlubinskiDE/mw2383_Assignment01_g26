230198b4418cf5db70999c4c4ca6ddfe2862af20
Selennsta <selennusta1@outlook.com>
2024-11-13 15:37:03 +0100

    Update p3.c

--- mw2383_Assignment01/P3_relational_Operators/p3.c
+++ mw2383_Assignment01/P3_relational_Operators/p3.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-	int var1, var2;
-    // Input values for var1 and var2
-    printf("Enter value for var1: ");
-    scanf("%d", &var1);
-    printf("Enter value for var2: ");
-    scanf("%d", &var2);
-
-    // Check if var1 is at least two times larger than var2
-    if (var1 >= 2 * var2) {
-        printf("Pasta\nis\nyummy.\n");
-    }
-    // Otherwise, check if var2 is more than three times larger than var1
-    // and if the remainder of var2 divided by var1 is less than 5
-    else if (var2 > 3 * var1 && (var2 % var1) < 5) {
-        printf("Fruits\nare\nhealthy.\n");
-    }
-    // In all other cases
-    else {
-        printf("Chocolate\nis\nsweet.\n");
-    }
+	
 	return 0;
 }
