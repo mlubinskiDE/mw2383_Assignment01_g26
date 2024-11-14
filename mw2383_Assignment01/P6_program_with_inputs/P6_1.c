b718068e6ed4c97c0c994a2f4e74f988ddb357b6
dCartoffel <daniel@csruediger.de>
2024-11-14 13:19:28 +0100

    First version, still wrong

--- mw2383_Assignment01/P6_program_with_inputs/P6_1.c
+++ mw2383_Assignment01/P6_program_with_inputs/P6_1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+//Program that takes an integer and a float variable as input from a user
+//Calculates multiplication of the two inputs and saves it to a variable.
+//Print out the variable’s value.
+
 int main(){
-	/*
-	Add solution here
-	*/
+	int integer;
+	float flo;
+
+	printf("Enter interger variable here: ");
+	scanf("%d", &integer);
+	printf("Enter float variable here: ");
+	scanf("%d", &flo);
+
+	float multiplication; //makes sense in my head but output doesn't
+	multiplication = integer * flo; //is there some type conversion afoot?
+	printf("Value of multiplication: %g", multiplication);
     
+	return 0;
 }
--- mw2383_Assignment01/P6_program_with_inputs/P6_2.c
+++ mw2383_Assignment01/P6_program_with_inputs/P6_2.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
+//Program that takes an integer input from a user for 3 times. 
+//For each time, the program prints “The number is greater than 6” if value greater 6; or prints “The number is less than 6” if number less 6; 
+//otherwise, it prints “The number is 6” if the input integer is 6 program must exit. 
+//For example, if the first input is “6”, the system will skip the following input and exit
+
 int main(void){
-    
-	/*
-	Add solution here
-	*/
-     return 0;
+	int track = 0;
+	int userInput;
+
+    while(track != 3) {
+		printf("Please enter a number here: ");
+		scanf("%d", &userInput);
+
+		if(userInput > 6) {
+			printf("The number is greater than 6.\n");
+			}
+		else if(userInput < 6){
+				printf("The number is smaller than 6.\n");
+			}
+		else{
+			printf("The number is 6."); //implement exit condition
+			return 0;
+			}
+		track++;
+	}
 }
 
