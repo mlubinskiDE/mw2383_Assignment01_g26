802f244feb7ec01019576b15421f22b95dfd5cfa
dCartoffel <daniel@csruediger.de>
2024-11-14 06:31:09 +0100

    Change variables to row & col for better read

--- mw2383_Assignment01/P1_Basics/p1_t3.c
+++ mw2383_Assignment01/P1_Basics/p1_t3.c
@@ -2,14 +2,11 @@
 
 //Write a program that prints integer multiplications in the range 0 to 10. 
 //(Only the multiplication results must be printed, the top row and left column is added to clarify the task; columns do not have to be aligned)
-//	 x ->
-// y
-// |
 
 int main(void) {
-	for(int y = 1; y <= 10; y++) {
-			for(int x = 1; x <= 10; x++) {
-				printf("%d\t", (y * x));
+	for(int row = 1; row <= 10; row++) {
+			for(int col = 1; col <= 10; col++) {
+				printf("%d\t", (row * col));
 			}
 			printf("\n");
 		}	
