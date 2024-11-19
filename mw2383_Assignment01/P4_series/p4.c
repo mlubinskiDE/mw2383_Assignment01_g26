0f83a20846394a7ba5fb7abfd452690c1517ff35
dCartoffel <daniel@csruediger.de>
2024-11-19 16:34:46 +0100

    Added task description

--- mw2383_Assignment01/P4_series/p4.c
+++ mw2383_Assignment01/P4_series/p4.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+//A very well-known series in Mathematics is the series where each new number F (n) is the result of the addition of the last two numbers of the fibonacci sequence 
+//F (n) = F (n − 1) + F (n − 2) with F (1) = 0, F (2) = 1
+//Your task is to calculate an altered version of that original series, which is instead mathematically defined as:
+//F (n) = F (n − 1) + F (n − 2) + F (n − 3) with F (1) = 0, F (2) = 0, F (3) = 1
+
+// Write a program that calculates this series series up to n==N with N as an integer variable that is set in your code
+
 int main(void) {
 	int N = 10; // Set the value of N as required
 	int F1 = 0, F2 = 0, F3 = 1; // Initial values F(1), F(2), F(3)
